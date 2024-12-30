1d15c33e7560497f84327b31672429f537aa0809
prakash.vidavaluru <prakash.vidavaluru@rackwareinc.com>
2024-12-30 22:02:13 +0530

    about changed

--- src/components/About.js
+++ src/components/About.js
@@ -21,9 +21,9 @@ const About = () => {
             quisquam harum nam cumque temporibus explicabo dolorum sapiente odio
             unde dolor?
           </p>
-          <a href="#" className="btn">
+          {/* <a href="#" className="btn">
             read more
-          </a>
+          </a> */}
         </article>
       </div>
     </section>
